437213de6f609ffdd8d59e7aad1055f383317158
David Kouril <david.kouril@gmail.com>
2022-05-20 12:21:57 +0200

    WIP: CPU implementation of max distance

--- libs/graphics/src/labeling/labelLayoutGenerator.ts
+++ libs/graphics/src/labeling/labelLayoutGenerator.ts
@@ -10,6 +10,9 @@ export type Label = {
     text: string;
 };
 
+const DOWNSCALED_TEX_SIZE = 512;
+// const DOWNSCALED_TEX_SIZE = 1024;
+
 export class LabelLayoutGenerator {
 
     private _viewport: ChromatinViewport | null = null;
@@ -20,6 +23,7 @@ export class LabelLayoutGenerator {
     private distanceTransformTexture: GPUTexture | null = null;
     private pingTexture: GPUTexture | null = null;
     private pongTexture: GPUTexture | null = null;
+    private smallIDTexture: GPUTexture | null = null;
 
     //~ internal state
     private lastFrameLabels: Label[] = [];
@@ -46,8 +50,8 @@ export class LabelLayoutGenerator {
         }
 
         const size = {
-            width: 512,
-            height: 512,
+            width: DOWNSCALED_TEX_SIZE,
+            height: DOWNSCALED_TEX_SIZE,
         }
 
         const usageFlags = GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.STORAGE_BINDING | GPUTextureUsage.TEXTURE_BINDING;
@@ -55,7 +59,9 @@ export class LabelLayoutGenerator {
         this.pingTexture = this.graphicsLibrary.device.createTexture({ label: "DT: Ping (Labeling)", size, format: format, usage: usageFlags});
         this.pongTexture = this.graphicsLibrary.device.createTexture({ label: "DT: Pong (Labeling)", size, format: format, usage: usageFlags});
         //~ TODO: this probably shouldn't be downscaled...
-        this.distanceTransformTexture = this.graphicsLibrary.device.createTexture({ label: "DT: Final (Labeling)", size, format: format, usage: usageFlags});
+        this.distanceTransformTexture = this.graphicsLibrary.device.createTexture({ label: "DT: Final (Labeling)", size, format: format, usage: usageFlags | GPUTextureUsage.COPY_SRC});
+        //~ just an experiment: I wanted to make a "ground truth" CPU implementation of max dist but from that I need both DT texture and ID textures as array
+        this.smallIDTexture = this.graphicsLibrary.device.createTexture({ label: "Downscaled ID Texture (Labeling)", size, format: format, usage: usageFlags | GPUTextureUsage.COPY_SRC});
     }
 
     public resizeTextures(width: number, height: number): void {
@@ -153,18 +159,22 @@ export class LabelLayoutGenerator {
         this.graphicsLibrary.blit(contoursSeedTex, this.pingTexture);
 
         //~ Compute DT steps
-        this.computeDTStep(this.pingTexture, this.pongTexture, 512.0 / 2.0);
-        this.computeDTStep(this.pongTexture, this.pingTexture, 512.0 / 4.0);
-        this.computeDTStep(this.pingTexture, this.pongTexture, 512.0 / 8.0);
-        this.computeDTStep(this.pongTexture, this.pingTexture, 512.0 / 16.0);
-        this.computeDTStep(this.pingTexture, this.pongTexture, 512.0 / 32.0);
-        this.computeDTStep(this.pongTexture, this.pingTexture, 512.0 / 64.0);
-        this.computeDTStep(this.pingTexture, this.pongTexture, 512.0 / 128.0);
-        this.computeDTStep(this.pongTexture, this.pingTexture, 512.0 / 256.0);
-        this.computeDTStep(this.pingTexture, this.pongTexture, 512.0 / 512.0);
+        const RES = DOWNSCALED_TEX_SIZE;
+        this.computeDTStep(this.pingTexture, this.pongTexture, RES / 2.0);
+        this.computeDTStep(this.pongTexture, this.pingTexture, RES / 4.0);
+        this.computeDTStep(this.pingTexture, this.pongTexture, RES / 8.0);
+        this.computeDTStep(this.pongTexture, this.pingTexture, RES / 16.0);
+        this.computeDTStep(this.pingTexture, this.pongTexture, RES / 32.0);
+        this.computeDTStep(this.pongTexture, this.pingTexture, RES / 64.0);
+        this.computeDTStep(this.pingTexture, this.pongTexture, RES / 128.0);
+        this.computeDTStep(this.pongTexture, this.pingTexture, RES / 256.0);
+        this.computeDTStep(this.pingTexture, this.pongTexture, RES / 512.0);
+        //~ debug
+        this.computeDTStep(this.pongTexture, this.pingTexture, RES / 1024.0);
 
         //~ copy result to final distance transform texture
-        this.graphicsLibrary.blit(this.pongTexture, distanceTransfromTex);
+        // this.graphicsLibrary.blit(this.pongTexture, distanceTransfromTex);
+        this.graphicsLibrary.blit(this.pingTexture, distanceTransfromTex);
     }
 
     public async getLabelPositions(): Promise<Label[]> {
@@ -179,6 +189,9 @@ export class LabelLayoutGenerator {
             return []; 
         }
         this.computeContours(idBuffer, this.contoursTexture);
+        //~ just an experiment:
+        if (!this.graphicsLibrary || !this.smallIDTexture) return [];
+        this.graphicsLibrary.blit(idBuffer, this.smallIDTexture);
 
         //~ distance transform
         if (!this.contoursTexture || !this.distanceTransformTexture) return [];
@@ -186,12 +199,14 @@ export class LabelLayoutGenerator {
 
         //~ max distance
         // this.computeMaxDistance();
-        const labels = await this.computeMaxDistance();
+        // const labels = await this.computeMaxDistance();
         // let labels = await this.computeMaxDistance();
+        const labelsCPU = await this.computeMaxDistanceCPU(this.distanceTransformTexture);
 
         //~ TODO: for now just returning a bunch of random labels
         // return this.debug_getRandomLabelPositions();
-        return labels;
+        // return labels;
+        return labelsCPU;
     }
 
     // #endregion
@@ -253,8 +268,8 @@ export class LabelLayoutGenerator {
         // console.log("stepSize = " + stepSize);
         const stepParams = {
             stepSize: stepSize, //~ do I need to somehow convert so that it's compatible with f32 in wgsl?
-            widthScale: this.viewport.width / 512.0,
-            heightScale: this.viewport.height / 512.0,
+            widthScale: this.viewport.width / DOWNSCALED_TEX_SIZE,
+            heightScale: this.viewport.height / DOWNSCALED_TEX_SIZE,
         };
         
           const stepParamBufferSize = 3 * Float32Array.BYTES_PER_ELEMENT;
@@ -290,8 +305,8 @@ export class LabelLayoutGenerator {
 
         // console.log("DT: dispatching workgroups!");
         passEncoder.dispatchWorkgroups(
-            512 / 8,
-            512 / 8,
+            DOWNSCALED_TEX_SIZE / 8,
+            DOWNSCALED_TEX_SIZE / 8,
             1);
         // passEncoder.dispatchWorkgroups(
         //     Math.ceil((parameters.width + 7) / 8),
@@ -349,7 +364,8 @@ export class LabelLayoutGenerator {
         const labelCandidates = new Float32Array(new ArrayBuffer(BUFFER_SIZE));
         for (let i = 0; i < MAX_LABELS; i++) {
             labelCandidates[i * 4 + 0] = -1; // regionId (i32)
-            labelCandidates[i * 4 + 1] = 0; // dtValue (f32)
+            // labelCandidates[i * 4 + 1] = 1.0; // dtValue (f32)
+            labelCandidates[i * 4 + 1] = 0.0; // dtValue (f32)
             labelCandidates[i * 4 + 2] = 0; // uvPosition.x (vec2<f32>)
             labelCandidates[i * 4 + 3] = 0; // uvPosition.y (vec2<f32>)
         }
@@ -383,8 +399,8 @@ export class LabelLayoutGenerator {
 
         // console.log("DT: dispatching workgroups!");
         passEncoder.dispatchWorkgroups(
-            512 / 8,
-            512 / 8,
+            DOWNSCALED_TEX_SIZE / 8,
+            DOWNSCALED_TEX_SIZE / 8,
             1);
 
         passEncoder.end();
@@ -428,12 +444,134 @@ export class LabelLayoutGenerator {
                 // break;
             } else {
                 labels.push(lbl);
+                console.log("regionId: %d, uvPosition: (%f, %f), dtValue: %f", regionId, x, y, dtValue);
             }
         }
         console.log("Labels:");
-        console.log(labels);
+        // console.log(labels);
 
         return labels;
+    }
+
+    private async computeMaxDistanceCPU(dtTexture: GPUTexture): Promise<Label[]> {
+
+        //~ TODO: copy dtTexture content to a buffer
+        const dtTextureContent = await this.getTextureAsArray(dtTexture);
+        if (!this.smallIDTexture) return [];
+        const smallIdTextureContent = await this.getTextureAsArray(this.smallIDTexture);
+
+        //~ TODO: loop over pixels and pick best candidates for each region ID
+        type Candidate = {
+            x: number,
+            y: number,
+            dtValue: number,
+            regionId: number,
+        }
+        const candidates = new Array<Candidate>(256);
+        //~ init
+        for (let c = 0; c < 256; c++) {
+            candidates[c] = {x: 0, y: 0, dtValue: 0, regionId: -1};
+        }
+
+        const t = dtTextureContent;
+        const tId = smallIdTextureContent;
+        const w = 512;
+        const h = 512;
+        for (let i = 0; i < 512; i++) {
+            for (let j = 0; j < 512; j++) {
+                const index = j * (h * 4) + i * 4;
+                const pixelVal = { 
+                    x: t[index + 0], 
+                    y: t[index + 1], 
+                    z: t[index + 2], 
+                    w: t[index + 3]
+                };
+                // const id = pixelVal.w;
+                const id = tId[index + 0];
+                const currentBest = candidates[id];
+                if (!currentBest) continue;
+                if (pixelVal.z > currentBest.dtValue) {
+                    const u = i / 512.0;
+                    const v = j / 512.0;
+                    // const newBest = {x: pixelVal.x, y: pixelVal.y, dtValue: pixelVal.z, regionId: id};
+                    const newBest = {x: u, y: v, dtValue: pixelVal.z, regionId: id};
+                    candidates[id] = newBest;
+                }
+            }
+        }
+
+        //~ debug
+        candidates[123] = {x: 0, y: 0, dtValue: 321, regionId: 123};
+        candidates[42] = {x: 0.5, y: 0.5, dtValue: 321, regionId: 42};
+        candidates[43] = {x: 1.0, y: 1.0, dtValue: 321, regionId: 43};
+
+        if (!this.viewport) return [];
+
+        const labels: Label[] = [];
+        for (let c = 0; c < 256; c++) {
+            const candidate = candidates[c];
+            if (candidate.regionId < 0) {
+                continue;
+            } else {
+                const xScreen = candidate.x * (this.viewport.width);
+                // const xScreen = candidate.x * (this.viewport.width / 2.0);
+                // const xScreen = candidate.x * (this.viewport.width / 4.0);
+                const yScreen = candidate.y * (this.viewport.height);
+                // const yScreen = candidate.y * (this.viewport.height / 2.0);
+                // const yScreen = candidate.y * (this.viewport.height / 4.0);
+
+                const lbl = {
+                    // x: candidate.x,
+                    x: xScreen,
+                    // y: candidate.y,
+                    y: yScreen,
+                    id: candidate.regionId,
+                    text: "Label TEST",
+                }
+                labels.push(lbl);
+            }
+        }
+            
+        // return [];
+        return labels;
+    }
+
+    private async getTextureAsArray(texture: GPUTexture): Promise<Float32Array> {
+        const empty = new Float32Array(0);
+        if (!this.graphicsLibrary) return empty; 
+
+        const device = this.graphicsLibrary.device;
+        const commandEncoder = device.createCommandEncoder();
+
+        const texWidth = 512;
+        const texHeight = 512;
+
+        const BUFFER_SIZE = texWidth * texHeight * 4 * 4;
+        const stagingBuffer = device.createBuffer({
+            size: BUFFER_SIZE,
+            usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST,
+        });
+
+        commandEncoder.copyTextureToBuffer(
+            { texture }, 
+            { buffer: stagingBuffer, bytesPerRow: texWidth * 4 * 4 }, 
+            [ texWidth, texHeight ]);
+        const commandBuffer = commandEncoder.finish();
+        device.queue.submit([commandBuffer]);
+
+        //~ reading back the buffer
+        await stagingBuffer.mapAsync(GPUMapMode.READ, 0, BUFFER_SIZE);
+        const copyArrayBuffer = stagingBuffer.getMappedRange(0, BUFFER_SIZE);
+        const data = copyArrayBuffer.slice(0);
+        stagingBuffer.unmap();
+        console.log("~~~ DT Texture dump: ~~~")
+        console.log(new Float32Array(data));
+        const dataArray = new Float32Array(data);
+
+        //~ dealloc
+        stagingBuffer.destroy();
+
+        return dataArray;
     }
 
     private debug_clearContoursTexture() {
