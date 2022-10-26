8d44e351c5ff0749493efbda2b4271aaf34eb14d
Matus Talcik <matus.talcik@outlook.com>
2022-10-26 16:03:37 +0200

    working tad selections

--- libs/graphics/src/viewports/distance_viewport.ts
+++ libs/graphics/src/viewports/distance_viewport.ts
@@ -208,7 +208,7 @@ export class DistanceViewport {
             colorAttachments: [
                 {
                     view: textureView,
-                    clearValue: { r: 1.0, g: 1.0, b: 1.0, a: 1.0 },
+                    clearValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
                     loadOp: 'clear',
                     storeOp: 'store',
                 },
